cc7587c764c82d59ff8f796cf2e26e37462dd7bf
Ashahad khan <ashahadk76@gmail.com>
2026-03-15 23:30:29 +0530

    add cv photo

--- src/component/cvdownload.js
+++ src/component/cvdownload.js
@@ -1,6 +1,6 @@
 export default function CVDownload() {
   return (
-    <a href="/Ashahad_Khan_CV1.pdf" download="Ashahad_Khan_CV1.pdf">
+    <a href="/Ashahad_Khan_CV 2.pdf" download="Ashahad_Khan_CV 2.pdf">
       Download CV
     </a>
   )
